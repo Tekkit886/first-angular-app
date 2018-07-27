73e12a71017be9c2e5f701573f84c26d471f47c7
Brian <brian@tailboardtech.com>
2018-07-26 23:35:29 -0400

    completed lesson 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SkillsComponent } from './skills/skills.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { DisplayMoviesComponent } from './modules/movies/display-movies/display-movies.component';
+import { DisplayMusicComponent } from './modules/music/display-music/display-music.component';
 
 const routes: Routes = [
 	{
@@ -19,10 +21,19 @@ const routes: Routes = [
 		component: GreetingComponent
 	},
 	{
+		path: 'movies',
+		component: DisplayMoviesComponent
+	},
+	{
+		path: 'movies',
+		component: DisplayMoviesComponent
+	},
+	{
 		path: '',
 		redirectTo: '/greeting',
 		pathMatch: 'full'
 	}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ClickMeComponent } from './click-me/click-me.component';
 import { ContactsModule } from './modules/contacts/contacts.module';
 import { AppRoutingModule } from './/app-routing.module';
 import { SkillsComponent } from './skills/skills.component';
+import { MoviesModule } from './modules/movies/movies.module';
+import { MusicModule } from './modules/music/music.module';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { SkillsComponent } from './skills/skills.component';
     FormsModule,
     ContactsModule,
     SkillsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MoviesModule,
+    MusicModule
   ],
   providers: [SkillsService],
   bootstrap: [AppComponent]
